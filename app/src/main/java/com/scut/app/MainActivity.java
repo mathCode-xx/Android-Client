a9a51120ac7e5c22a22e512f693533b50fc79296
徐鑫 <1589039158@qq.com>
2022-07-31 18:55:36 +0800

    init commit

---
+++ app/src/main/java/com/scut/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.scut.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.scut.bbs.BbsMainActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.bt_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BbsMainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ bbs/src/main/java/com/scut/bbs/BbsMainActivity.java
@@ -0,0 +1,14 @@
+package com.scut.bbs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BbsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbs_main);
+    }
+}
